4b116c8ff315ed1a4cee00427ed7862d0a4de1f0
Gonzalo <gnpantoja@outlook.com>
2022-08-24 11:38:07 -0500

    Revert "Merge pull request #2 from ashastr/theme"
    This reverts commit af3c3aba2e205be1a3fb166ee59c7389cdb07952, reversing
    changes made to 5aeeefb81f0e8c8975a070e31dbcea3fe76c39ee.

--- track/htc/mobile/src/app/evernym-sdk/startup.js
+++ track/htc/mobile/src/app/evernym-sdk/startup.js
@@ -4,10 +4,8 @@
  * Here is you can customize Startup wizard.
  * */
 
-// TODO: none of this works
-
 // image to use as a background
-export const BACKGROUND_IMAGE = require("../Discover/img/login.png");
+export const BACKGROUND_IMAGE = null
 
 // custom component to use for Start Up screen rendering (instead of predefined one)
-export const CustomStartUpScreen = () => <Text>Custom Start Up</Text>;
+export const CustomStartUpScreen = null
